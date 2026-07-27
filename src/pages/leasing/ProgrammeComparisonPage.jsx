43490b78415f85d6875f18777eb01718466b1c0f
mo-seoudi <moseoudi@hotmail.com>
2026-07-27 11:28:37 +0400

    Add ProgrammeComparisonPage component

---
+++ src/pages/leasing/ProgrammeComparisonPage.jsx
@@ -0,0 +1,12 @@
+export default function ProgrammeComparisonPage() {
+  return (
+    <section>
+      <h1>Programme Comparison</h1>
+      <p>
+        Compare several programmes within one selected academic year.
+      </p>
+
+      {/* Programme comparison table and chart will go here */}
+    </section>
+  );
+}
