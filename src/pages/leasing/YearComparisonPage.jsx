be2354501665ce93828b09c37baa862aee93fcf1
mo-seoudi <moseoudi@hotmail.com>
2026-07-27 11:28:20 +0400

    Add YearComparisonPage component for year comparison

---
+++ src/pages/leasing/YearComparisonPage.jsx
@@ -0,0 +1,12 @@
+export default function YearComparisonPage() {
+  return (
+    <section>
+      <h1>Year-on-Year Comparison</h1>
+      <p>
+        Compare one selected programme across multiple academic years.
+      </p>
+
+      {/* Year comparison table and chart will go here */}
+    </section>
+  );
+}
