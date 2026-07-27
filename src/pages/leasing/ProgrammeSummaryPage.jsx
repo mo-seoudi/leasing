e07c915016859842969f7ced6ce5605e41018c85
mo-seoudi <moseoudi@hotmail.com>
2026-07-27 11:27:58 +0400

    Create ProgrammeSummaryPage.jsx

---
+++ src/pages/leasing/ProgrammeSummaryPage.jsx
@@ -0,0 +1,12 @@
+export default function ProgrammeSummaryPage() {
+  return (
+    <section>
+      <h1>Programme Summary</h1>
+      <p>
+        Monthly and annual figures for one selected programme and academic year.
+      </p>
+
+      {/* Excel-style summary table will go here */}
+    </section>
+  );
+}
