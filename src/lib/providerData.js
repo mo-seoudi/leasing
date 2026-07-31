740b7b2d5aab47cc8ee8215c2110c246d1986b0a
mo-seoudi <moseoudi@hotmail.com>
2026-07-31 12:10:50 +0400

    Remove deposit field from provider data

--- src/lib/providerData.js
+++ src/lib/providerData.js
@@ -66,7 +66,6 @@ export function getProviderByName(providerName) {
       rentalFees: null,
       revenueCollection: "",
       invoiceFrequency: "",
-      deposit: null,
       schools: [],
     },
   };
