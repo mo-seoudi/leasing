056d6015d41e337049632753f89131976087d240
mo-seoudi <moseoudi@hotmail.com>
2026-08-18 12:59:32 +0400

    Rename data entry route to financial records

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,18 +4,13 @@ import ProtectedRoute from "../auth/ProtectedRoute";
 import PlatformLayout from "../layouts/PlatformLayout";
 
 import LoginPage from "../pages/auth/LoginPage";
-
 import DashboardPage from "../pages/dashboard/DashboardPage";
-
 import ProgrammeDirectoryPage from "../pages/leasing/ProgrammeDirectoryPage";
 import YearComparisonPage from "../pages/leasing/YearComparisonPage";
-
 import CateringDashboardPage from "../pages/catering/CateringDashboardPage";
 import CateringComparisonPage from "../pages/catering/CateringComparisonPage";
-
 import UniformDashboardPage from "../pages/uniform/UniformDashboardPage";
 import UniformComparisonPage from "../pages/uniform/UniformComparisonPage";
-
 import DataEntryPage from "../pages/admin/DataEntryPage";
 import SettingsPage from "../pages/settings/SettingsPage";
 
@@ -32,81 +27,49 @@ export default function AppRoutes() {
           </ProtectedRoute>
         }
       >
-        <Route
-          index
-          element={<Navigate to="/dashboard" replace />}
-        />
-
-        <Route
-          path="dashboard"
-          element={<DashboardPage />}
-        />
-
-        <Route
-          path="catering"
-          element={<CateringDashboardPage />}
-        />
-
+        <Route index element={<Navigate to="/dashboard" replace />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="catering" element={<CateringDashboardPage />} />
         <Route
           path="catering/comparison"
           element={<CateringComparisonPage />}
         />
-
-        <Route
-          path="uniform"
-          element={<UniformDashboardPage />}
-        />
-
+        <Route path="uniform" element={<UniformDashboardPage />} />
         <Route
           path="uniform/comparison"
           element={<UniformComparisonPage />}
         />
-
         <Route
           path="leasing/programmes"
           element={<ProgrammeDirectoryPage />}
         />
-
         <Route
           path="leasing/year-comparison"
           element={<YearComparisonPage />}
         />
-
         <Route
           path="leasing/summary"
-          element={
-            <Navigate
-              to="/leasing/programmes"
-              replace
-            />
-          }
+          element={<Navigate to="/leasing/programmes" replace />}
         />
-
         <Route
           path="leasing/program-comparison"
-          element={
-            <Navigate
-              to="/leasing/programmes"
-              replace
-            />
-          }
+          element={<Navigate to="/leasing/programmes" replace />}
         />
 
         <Route
-          path="data-entry"
+          path="financial-records"
           element={<DataEntryPage />}
         />
 
         <Route
-          path="settings"
-          element={<SettingsPage />}
+          path="data-entry"
+          element={<Navigate to="/financial-records" replace />}
         />
+
+        <Route path="settings" element={<SettingsPage />} />
       </Route>
 
-      <Route
-        path="*"
-        element={<Navigate to="/dashboard" replace />}
-      />
+      <Route path="*" element={<Navigate to="/dashboard" replace />} />
     </Routes>
   );
 }
